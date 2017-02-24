58c95f3ddd1f0961e20fbe1ce9d39cbb4b0e7772
Martino Facchin <m.facchin@arduino.cc>
2017-02-24 15:23:02 +0100

    fix darwin cross compilation

--- adb/compile.sh
+++ adb/compile.sh
@@ -11,4 +11,4 @@ CXX=i686-w64-mingw32-g++ CC=i686-w64-mingw32-gcc make -f Makefile_windows
 #osx
 export PATH=/opt/osxcross/target/bin/:$PATH
 export MACOSX_DEPLOYMENT_TARGET=10.9
-CXX=o64-clang++ CC=o64-clang++ make -f Makefile_darwin
+CXX=o64-clang++ CC=o64-clang make -f Makefile_darwin
