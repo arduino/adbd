fe8a035f43a5c966650c87d590bd54f683306101
Martino Facchin <m.facchin@arduino.cc>
2017-09-04 11:47:00 +0200

    fix watcher with infinite retry

--- watcher.sh
+++ watcher.sh
@@ -1,44 +1,58 @@
 #!/bin/bash
 modprobe dwc3
 modprobe dwc3-pci
-modprobe configfs 
-modprobe libcomposite 
-modprobe usb_f_serial 
-modprobe usb_f_fs 
+modprobe configfs
+modprobe libcomposite
+modprobe usb_f_serial
+modprobe usb_f_fs
 #insmod /root/usb_f_fs.ko
-modprobe usb_f_acm 
-sleep 2 
-cd /sys/kernel/config/usb_gadget/
-mkdir -p g1 
-cd g1 
-echo 0x8087 > idVendor 
-echo 0x101e > idProduct 
-mkdir strings/0x409 
-echo 012345678 > strings/0x409/serialnumber 
-echo Intel > strings/0x409/manufacturer 
-echo Joule > strings/0x409/product 
-mkdir -p functions/acm.GS0 
-mkdir -p functions/ffs.adb 
-mkdir -p configs/c.1 
-mkdir -p configs/c.1/strings/0x409 
-echo "adb+cdc" > configs/c.1/strings/0x409/configuration 
-echo 120 > configs/c.1/MaxPower 
-ln -s functions/acm.GS0 configs/c.1 
-ln -s functions/ffs.adb/ configs/c.1 
-mkdir -p /dev/usb-ffs 
-mkdir -p /dev/usb-ffs/adb 
-mount -t functionfs adb /dev/usb-ffs/adb 
-adbd & 
+modprobe usb_f_acm
 sleep 2
-set -x
+cd /sys/kernel/config/usb_gadget/
+if [ ! -d /sys/kernel/config/usb_gadget/g1 ]; then
+mkdir g1
+cd g1
+echo 0x8087 > idVendor
+echo 0x101e > idProduct
+mkdir strings/0x409
+echo 012345678 > strings/0x409/serialnumber
+echo Intel > strings/0x409/manufacturer
+echo IoTplatform > strings/0x409/product
+mkdir functions/acm.GS0
+mkdir functions/ffs.adb
+mkdir configs/c.1
+mkdir configs/c.1/strings/0x409
+echo "adb+cdc" > configs/c.1/strings/0x409/configuration
+echo 120 > configs/c.1/MaxPower
+ln -s functions/acm.GS0 configs/c.1
+ln -s functions/ffs.adb/ configs/c.1
+mkdir /dev/usb-ffs
+mkdir /dev/usb-ffs/adb
+mount -t functionfs adb /dev/usb-ffs/adb
+adbd &
+sleep 2
+fi
+while true; do
 UDC_VAL=`ls /sys/class/udc`
 UDC_VAL=$(echo $UDC_VAL|tr -d '\n')
-echo $UDC_VAL > UDC
-if [ $? != 0 ]; then
-    exit $?
+UDC_MOUNTED=`cat /sys/kernel/config/usb_gadget/g1/UDC`
+UDC_MOUNTED=$(echo $UDC_MOUNTED|tr -d '\n')
+if [ x$UDC_MOUNTED == x ]; then
+echo $UDC_VAL > /sys/kernel/config/usb_gadget/g1/UDC
+if [ $? == 0 ]; then
+echo "USB configured"
+sleep 10
 else
-while true; do
-    sleep 100
-done
+echo "USB configuration failed"
+sleep 1
 fi
-
+else
+if [ x$UDC_VAL == x ]; then
+echo "USB detached, sleeping"
+sleep 1
+else
+echo "USB already configured, sleeping"
+sleep 10
+fi
+fi
+done
